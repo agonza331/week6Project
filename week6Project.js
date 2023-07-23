cf58294eec02f0b3da8ceb8e52b0716271d3218f
Anayeli Gonzalez <anagonzalez1331>
2023-07-22 23:38:02 -0400

    updated week6Project

--- week6Project.js
+++ week6Project.js
@@ -19,7 +19,7 @@ class Card {
   }
   
   class Deck {
-    constructor(card, createDeck, shuffle) {
+    constructor() {
       this.cards= [];
       this.createDeck();
       this.shuffle();
@@ -36,14 +36,15 @@ class Card {
     }
   // looked up info for shuffle, still a little confused about j
   //this.card i & j = this.card i & j since it shuffles the length of last card which is 52 cards in total
-  //it will equal the same amount based off of i(52 cards)& j (return the lowest number thats give (A:11)
+  //it will equal the same amount based off of i(52 cards)& j (return the lowest number thats given)
   // out of 52 cards) so return any cards but same amount of cards to opponent 
 
-  //******* figure out if this is correct??? */
   shuffle() {
       for (let i = this.cards.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [this.cards[i], this.cards[j] = this.cards[j], this.cards[i]];
+        let j = Math.floor(Math.random() * (i-1));
+        let temp = this.cards[i];
+        this.cards[i] = this.cards[j];
+        this.cards[j] = temp
       }
     }
 
@@ -54,7 +55,7 @@ class Card {
   }
   // need players so given class, name of player, hand array since they will have cards, and points
   class Player {
-    constructor(name, hand, points) {
+    constructor(name) {
       this.name =name;
       this.hand = [];
       this.points = 0;
@@ -62,40 +63,62 @@ class Card {
   }
   let player1 = new Player('Player 1');
   let player2 = new Player('Player 2');
-  playHand(player1, player2);
+//player1 and 2 are instance of player-adding to the class/adding 2 new players
 
   //function for something to happen (play hand with the two players)
   // let deck of cards be from class deck
-  function playHand( player1, player2) {
+  function playHand(player1, player2) {
     let deck = new Deck();
-  //out of 52 cards each player gets 26, push-player given a deal of 26 cards from deck of out 52 cards
+  //out of 52 cards each player gets 26, push-player given a deal of 26 cards (i<26=26cards))
     for (let i = 0; i < 26; i++) {
       player1.hand.push(deck.deal());
       player2.hand.push(deck.deal());
     }
-  //pop each time for 26 cards
-    for (let i = 0; i < 26; i++) {
+  
+  //pop last card each time for 26 cards
+    for (let i = 0; i <26; i++) {
       let card1 = player1.hand.pop();
+
       let card2 = player2.hand.pop();
+      
   // if card1 is bigger than card 2 then player 1 gets a pont or if other way around then player 2 gets a point 
       if (card1.value > card2.value) {
+        console.log(`Player 1 played ${card1.cardValue} of ${card1.suit} and Player 2 played
+        ${card2.cardValue} of ${card2.suit}. Player 1 wins!`)
         player1.points++;
-      } else if (card1.value < card2.value) {
+      } if (card1.value < card2.value) {
+        console.log(`Player 2 played ${card2.cardValue} of ${card2.suit} and Player 1 played
+        ${card1.suit}. Player 2 wins!`);
         player2.points++;
+      } else if (card1.value===card2.value){
+        console.log(`Player 1 played ${card1.cardValue} of ${card1.suit} and Player 2 played 
+        ${card2.cardValue} of ${card2.suit}. It's a tie, no point.`)
       }
+      //I am a visual person so I need to make sure there was 26 rounds, added if there was a tie, no points
     }
+}
   // out of the whole function console.log the score for each player
-    console.log(`Player 1's score: ${player1.points}`);
-    console.log(`Player 2's score: ${player2.points}`);
-  }
+  playHand(player1, player2)  
+  console.log(`Player 1's score: ${player1.points}`);
+  console.log(`Player 2's score: ${player2.points}`);
   
-
+  //console.log('Leftover cards in player 1 hand', player1.hand)
+  //console.log('Letfover cards in player 2 hand', player2.hand)
+    //not needed but made sure that all cards were used.
+  
+  
+//display who wins the game at the end.
   if (player1.points>player2.points){
     console.log ('Player 1 is the winner.')
+  
   } if (player1.points<player2.points){
     console.log('Player 2 is the winner.')
+    
   } else if (player1.points===player2.points){
     console.log('There is no winner.')
+    
   } 
 
-  ///**** why is the output only 24 and 2 and player 1 is the winner? */
+
+  
+  
--- week6_test.js
+++ week6_test.js
@@ -1,7 +1,10 @@
 let expect=chai.expect;
-let Card = require('Card');
-let Deck = require('Deck');
-let Player = require('Player');
+
+let deck1= new Deck();
+console.log(deck1);
+let deck2= new Deck();
+console.log(deck2);
+
 
 describe('Card', function () {
     
@@ -9,17 +12,30 @@ describe('Card', function () {
 
 describe('Deck', function () {
     describe ('createDeck', function(){
-        it ('create a deck of cards using values and strings given', function(){
+        it ('creating deck to make sure length is 52 cards', function(){
+            expect(deck1.cards.length).to.equal(52)
+            expect(deck2.cards.length).to.equal(52)
         })
      })
-    describe ('shuffle', function(){
-    })
-        it ('should shuffle cards', function (){
+    describe ('create a deck the same way multiple times', function(){
+        it ('create a 52 card deck', function (){
+            expect (deck1.cards).to.deep.equal(deck2.cards)
+            expect (deck1.cards).to.deep.equal(deck2.cards);
+            console.log(deck1)
+            console.log(deck2)
         })
+    })
+        
+})
     describe ('deal', function(){
         it ('should deal a hand of cards', function(){
+            expect(deck.deal()).to.be.an.instanceof(Card)
         })
-    })
+        it ('should deal a hand with 26 cards', function(){
+            let test = deck1.deal();
+            expect(test.length).to.equal(26);
+            console.log(test)
+        })
 });
 
 describe('Player', function () {
